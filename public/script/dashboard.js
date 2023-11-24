b90da4a6c16a27dedab6593565616615634be6f2
akmal <akmalfaditya@gmail.com>
2023-11-24 16:22:03 +0700

    update 404 page

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,30 +1,20 @@
 const mobileScreen = window.matchMedia("(max-width: 990px)");
 
 $(document).ready(function () {
-    $(".dashboard-nav-dropdown-toggle").click(function () {
-        $(this).closest(".dashboard-nav-dropdown")
-            .toggleClass("show")
-            .find(".dashboard-nav-dropdown")
-            .removeClass("show");
-        $(this).parent()
-            .siblings()
-            .removeClass("show");
-    });
-
     $(".menu-toggle").click(function () {
         if (mobileScreen.matches) {
             // Slide down/up animation for mobile menu toggle
-            $(".dashboard-nav").toggleClass("mobile-show").slideToggle(300); // Adjust the duration as needed
+            $(".dashboard-nav").slideToggle(300); // Adjust the duration as needed
         } else {
             $(".dashboard").toggleClass("dashboard-compact");
 
             // Slide animation for the dashboard content
             $(".dashboard-app").animate({
                 marginLeft: $(".dashboard").hasClass("dashboard-compact") ? "0px" : "300px"
-            }, 300); // Adjust the duration as needed
+            }, 300);
             $(".dashboard-nav").animate({
                 marginLeft: $(".dashboard").hasClass("dashboard-compact") ? "-300px" : "0px"
-            }, 300); 
+            }, 300);
         }
     });
 });
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,6 @@
 @section('content')
     <!-- Page Content -->
     <div class="size-404">
-
         <main>
             <div class="container">
                 <div class="row">
@@ -17,11 +16,11 @@
                     <div class="col-md-6 align-self-center">
                         <h1 class="h1-404">404</h1>
                         <h2 class="h2-404">UH OH! You're lost.</h2>
-                        <p>The page you are looking for does not exist.
+                        <p class="p-404">The page you are looking for does not exist.
                             How you got here is a mystery. But you can click the button below
                             to go back to the homepage.
                         </p>
-                        <a href="https://webcareidn.com/">
+                        <a class="a-404" href="https://webcareidn.com/">
                             <button class="btn-404 dark">HOME</button>
                         </a>
                     </div>
