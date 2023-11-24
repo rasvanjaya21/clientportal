d83f1a8cdde5fb8a6130854754a27b98b83655d9
akmal <akmalfaditya@gmail.com>
2023-11-24 13:51:23 +0700

    responsive dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -114,26 +114,28 @@
             <div class="card shadow border-0 card-important bg-white mb-3 p-0">
                 <div class="card-title p-3" style="background-color: #0560fd; border-radius: 10px 10px 0px 0px;">
                     {{-- <h5 class="m-0 font-weight-bold">Notifications</h5> --}}
+                    <div class="container p-0 m-0">
+                        <div class="row">
+                            <div class="col-8 d-flex justify-content-start align-items-center">
+                                <h5 class="m-0 font-weight-bold text-white">Recent Comments</h5>
+                            </div>
 
-                    <div class="row">
-                        <div class="col-md-6 d-flex justify-content-start">
-                            <h5 class="m-0 font-weight-bold text-white">Recent Comments</h5>
-                        </div>
-                        <div class="col-md-2 ">
 
-                        </div>
-                        <div class="col-md-4 d-flex justify-content-end align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512">
-                                <style>
-                                    svg {
-                                        fill: white
-                                    }
-                                </style>
-                                <path
-                                    d="M168.2 384.9c-15-5.4-31.7-3.1-44.6 6.4c-8.2 6-22.3 14.8-39.4 22.7c5.6-14.7 9.9-31.3 11.3-49.4c1-12.9-3.3-25.7-11.8-35.5C60.4 302.8 48 272 48 240c0-79.5 83.3-160 208-160s208 80.5 208 160s-83.3 160-208 160c-31.6 0-61.3-5.5-87.8-15.1zM26.3 423.8c-1.6 2.7-3.3 5.4-5.1 8.1l-.3 .5c-1.6 2.3-3.2 4.6-4.8 6.9c-3.5 4.7-7.3 9.3-11.3 13.5c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c5.1 0 10.2-.3 15.3-.8l.7-.1c4.4-.5 8.8-1.1 13.2-1.9c.8-.1 1.6-.3 2.4-.5c17.8-3.5 34.9-9.5 50.1-16.1c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9zM144 272a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm144-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm80 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" />
-                            </svg>
+
+                            <div class="col-4 d-flex justify-content-end  align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512">
+                                    <style>
+                                        svg {
+                                            fill: white
+                                        }
+                                    </style>
+                                    <path
+                                        d="M168.2 384.9c-15-5.4-31.7-3.1-44.6 6.4c-8.2 6-22.3 14.8-39.4 22.7c5.6-14.7 9.9-31.3 11.3-49.4c1-12.9-3.3-25.7-11.8-35.5C60.4 302.8 48 272 48 240c0-79.5 83.3-160 208-160s208 80.5 208 160s-83.3 160-208 160c-31.6 0-61.3-5.5-87.8-15.1zM26.3 423.8c-1.6 2.7-3.3 5.4-5.1 8.1l-.3 .5c-1.6 2.3-3.2 4.6-4.8 6.9c-3.5 4.7-7.3 9.3-11.3 13.5c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c5.1 0 10.2-.3 15.3-.8l.7-.1c4.4-.5 8.8-1.1 13.2-1.9c.8-.1 1.6-.3 2.4-.5c17.8-3.5 34.9-9.5 50.1-16.1c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9zM144 272a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm144-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm80 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+
                 </div>
                 <div class="card-body p-0">
 
@@ -166,25 +168,26 @@
             <div class="card shadow border-0 card-important bg-white mb-3 p-0">
                 <div class="card-title p-3" style="background-color: #0560fd; border-radius: 10px 10px 0px 0px;">
                     {{-- <h5 class="m-0 font-weight-bold">Recent Clients</h5> --}}
-                    <div class="row">
-                        <div class="col-md-6 d-flex justify-content-start">
-                            <h5 class="m-0 font-weight-bold text-white">Recent Clients</h5>
-                        </div>
-                        <div class="col-md-2 ">
+                    <div class="container p-0 m-0">
+                        <div class="row">
+                            <div class="col-8 d-flex justify-content-start align-items-center">
+                                <h5 class="m-0 font-weight-bold text-white">Recent Clients</h5>
+                            </div>
 
-                        </div>
-                        <div class="col-md-4 d-flex justify-content-end align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
-                                <style>
-                                    svg {
-                                        fill: #ffffff
-                                    }
-                                </style>
-                                <path
-                                    d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
-                            </svg>
+                            <div class="col-4 d-flex justify-content-end align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
+                                    <style>
+                                        svg {
+                                            fill: #ffffff
+                                        }
+                                    </style>
+                                    <path
+                                        d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+
                 </div>
                 <div class="card-body p-0">
 
