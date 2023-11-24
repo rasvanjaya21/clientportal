028f863f1dd901e515a2b6f6dea90599b9589742
Halim <halimputra0701@gmail.com>
2023-11-25 03:09:43 +0700

    reformat js, update reuse toggle func n event list

--- public/script/signinup.js
+++ public/script/signinup.js
@@ -1,66 +1,67 @@
-function togglePasswordVisibility(passwordInputId, showPasswordId, hidePasswordId) {
-    const passwordInput = document.getElementById(passwordInputId);
-    const showPassword = document.getElementById(showPasswordId);
-    const hidePassword = document.getElementById(hidePasswordId);
+// VARIABLE DECLARATION
+const passwordInput = document.getElementById("passwordInput");
+const showPasswordIcon = document.getElementById("showPassword");
+const hidePasswordIcon = document.getElementById("hidePassword");
 
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        showPassword.style.display = 'none';
-        hidePassword.style.display = 'inline';
+const createPasswordInput = document.getElementById("passwordInputRegCre");
+const showCreatePasswordIcon = document.getElementById("showPasswordRegCre");
+const hideCreatePasswordIcon = document.getElementById("hidePasswordRegCre");
+
+const confirmPasswordInput = document.getElementById("passwordInputReg");
+const showConfirmPasswordIcon = document.getElementById("showPasswordReg");
+const hideConfirmPasswordIcon = document.getElementById("hidePasswordReg");
+// FUNCTION DECALARATION
+function togglePasswordVisibility(passwordInputId, showPasswordIconId, hidePasswordIconId) {
+
+	const passwordInput = document.getElementById(passwordInputId);
+    const showPasswordIcon = document.getElementById(showPasswordIconId);
+    const hidePasswordIcon = document.getElementById(hidePasswordIconId);
+
+    // TOGGLE PASSWORD USECASE 1 - toggle password to text, show icon to hide icon
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPasswordIcon.style.display = "inline";
+        hidePasswordIcon.style.display = "none";
+
+	// TOGGLE PASSWORD USECASE 2 - rollback text to password, hide icon to show icon
     } else {
-        passwordInput.type = 'password';
-        showPassword.style.display = 'inline';
-        hidePassword.style.display = 'none';
+        passwordInput.type = "password";
+        showPasswordIcon.style.display = "none";
+        hidePasswordIcon.style.display = "inline";
     }
-}
 
-function togglePasswordIcon(passwordInputId, showPasswordId, hidePasswordId) {
-    const passwordInput = document.getElementById(passwordInputId);
-    const showPassword = document.getElementById(showPasswordId);
-    const hidePassword = document.getElementById(hidePasswordId);
-
-    if (passwordInput.value !== '') {
-        passwordInput.type = 'password';
-        showPassword.style.display = 'inline';
-        hidePassword.style.display = 'none';
-    } else {
-        passwordInput.type = 'text';
-        showPassword.style.display = 'none';
-        hidePassword.style.display = 'inline';
-    }
 }
 
 const toggleForm = (page) => {
-    const container = document.querySelector('.container-signinup');
-    container.classList.toggle('active');
+    const container = document.querySelector(".container-signinup");
+    container.classList.toggle("active");
     window.location.href = page;
 };
 
-// Check if the login page elements exist and attach event listeners
-const passwordInput = document.getElementById('passwordInput');
-const showPassword = document.getElementById('showPassword');
-const hidePassword = document.getElementById('hidePassword');
+// EVENT LISTENER
+passwordInput?.addEventListener("input", (event) => {
+    // TOGGLE PASSWORD USECASE 3 : rollback empty input `input-password` to default behavior (as need)
+    if (event.target.value == "") {
+        passwordInput.type = "password";
+        showPasswordIcon.style.display = "none";
+        hidePasswordIcon.style.display = "inline";
+    }
+});
 
-if (passwordInput && showPassword && hidePassword) {
-    passwordInput.addEventListener('input', () => togglePasswordIcon('passwordInput', 'showPassword', 'hidePassword'));
-    togglePasswordIcon('passwordInput', 'showPassword', 'hidePassword');
-}
+createPasswordInput?.addEventListener("input", (event) => {
+    // TOGGLE PASSWORD USECASE 4 : rollback empty input `create-password` to default behavior (as need)
+    if (event.target.value == "") {
+        createPasswordInput.type = "password";
+        showCreatePasswordIcon.style.display = "none";
+        hideCreatePasswordIcon.style.display = "inline";
+    }
+});
 
-// Check if the register page elements exist and attach event listeners
-const passwordInputRegCre = document.getElementById('passwordInputRegCre');
-const showPasswordRegCre = document.getElementById('showPasswordRegCre');
-const hidePasswordRegCre = document.getElementById('hidePasswordRegCre');
-
-if (passwordInputRegCre && showPasswordRegCre && hidePasswordRegCre) {
-    passwordInputRegCre.addEventListener('input', () => togglePasswordIcon('passwordInputRegCre', 'showPasswordRegCre', 'hidePasswordRegCre'));
-    togglePasswordIcon('passwordInputRegCre', 'showPasswordRegCre', 'hidePasswordRegCre');
-}
-
-const passwordInputReg = document.getElementById('passwordInputReg');
-const showPasswordReg = document.getElementById('showPasswordReg');
-const hidePasswordReg = document.getElementById('hidePasswordReg');
-
-if (passwordInputReg && showPasswordReg && hidePasswordReg) {
-    passwordInputReg.addEventListener('input', () => togglePasswordIcon('passwordInputReg', 'showPasswordReg', 'hidePasswordReg'));
-    togglePasswordIcon('passwordInputReg', 'showPasswordReg', 'hidePasswordReg');
-}
+confirmPasswordInput?.addEventListener("input", (event) => {
+    // TOGGLE PASSWORD USECASE 5 : rollback empty input `confirm-password` to default behavior (as need)
+    if (event.target.value == "") {
+        confirmPasswordInput.type = "password";
+        showConfirmPasswordIcon.style.display = "none";
+        hideConfirmPasswordIcon.style.display = "inline";
+    }
+});
